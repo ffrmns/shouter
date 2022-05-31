38a58b11f5f40edf7071cf313c59bd934d5511aa
ffrmns <fikrifa0@gmail.com>
2022-06-01 06:36:26 +0700

    Add dependencies: java.jdbc and postgresql

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
 (defproject shouter "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.3"]]
+  :description "Shouter app"
+  :url "http://github.com/ffrmns/shouter"
+  :license {:name "Eclipse Public License"
+            :url "https://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [org.clojure/java.jdbc "0.7.12"]
+                 [org.postgresql/postgresql "42.3.6"]]
   :repl-options {:init-ns shouter.core})
