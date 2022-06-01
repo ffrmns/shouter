8a7708653dad797eec48b98d631d3cc8d112e4d0
ffrmns <fikrifa0@gmail.com>
2022-06-02 06:47:31 +0700

    Add :min-lein-version in project.clj for heroku

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://github.com/ffrmns/shouter"
   :license {:name "Eclipse Public License"
             :url "https://www.eclipse.org/legal/epl-v10.html"}
+  :min-lein-version "2.9.8"
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [org.clojure/java.jdbc "0.7.12"]
                  [org.postgresql/postgresql "42.3.6"]
